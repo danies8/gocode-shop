c2db59791166df3e23d610f95f5d91a692471a39
Dani Shitrit <yael_b4@yahoo.com>
2021-01-26 19:44:52 +0200

    New commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-   title = "bla";
+   title = "bla;}";
 
   OnToggle ():void{
     this.title = this.title =="" ? "bla" :"";
