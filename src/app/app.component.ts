3a632c24d2836946292428d0512330c8bffcc799
Dani Shitrit <yael_b4@yahoo.com>
2021-01-26 20:04:36 +0200

    Change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-   title = "bla;}";
+   title = "bla   123;}";
 
   OnToggle ():void{
     this.title = this.title =="" ? "bla" :"";
